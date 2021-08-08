38329f7346c8617af3edc6817906da49c9c456de
saifur111 <56952260+saifur111@users.noreply.github.com>
2021-08-08 18:40:37 +0600

    complete only 12

--- assignment-pre.js
+++ assignment-pre.js
@@ -1,6 +1,7 @@
 /*-------------------------------------
     variable declaration ==== 1
 --------------------------------------*/
+
 var myName ="Md Saifur Rahman"  // string variable
 console.log(myName);
 console.log(typeof(myName));   // string
@@ -68,3 +69,166 @@ var age =20;
 let voteable = (age < 18) ? "Not Allowed !":"Allowed"; // <
 console.log('Your are ',voteable ,'to vote.');
 
+var isTrue = 2 > "saifur";//false
+console.log(isTrue);
+
+var isFalse = 2 == 2;//true
+console.log(isFalse);
+//Greater than or equal (>=)
+console.log('ab' >= 'aa');// expected output: true
+//Less than or equal (<=)
+console.log(3333 <= 3333);// expected output: true
+//Not equal (!=)
+console.log(3333 != 3333);// expected output: false
+
+const a = 20;
+const b = -20;
+//Logical AND (&&)
+console.log(a > 0 && b > 0);// expected output: false
+//Logical OR (||)
+console.log(a > 0 || b > 0);// expected output: true
+//Logical NOT (!)
+console.log(!(a > 0 || b > 0));// expected output: false
+/*-------------------------------------
+      Comparison Operators  ==== 5
+--------------------------------------*/
+function testNumber(a) {
+  let result;
+  if (a > 0 && a<=1000) {
+    result = 'positive and lessthen equal 1000.';
+  }
+  else if(a>1000){
+    result = 'larger then  1000.';
+  } 
+  else {
+    result = 'NOT positive';
+  }
+  return result;
+}
+
+console.log(testNumber(-5));// expected output: "NOT positive"
+/*-------------------------------------
+      if-else  ==== 4
+--------------------------------------*/
+var num1= Math.round(Math.random()*10);
+var num2= Math.round(Math.random()*10);
+function twoConditions(n1, n2) {
+    if (n1 > 0 && n2 < 5) {
+      console.log("Fulfill both conditions",n1,n2);
+    } else {
+      console.log("doesn't fulfill both conditions",n1,n2);
+    }
+  }
+  console.log(twoConditions(num1, num2));
+/*-------------------------------------
+      while loop
+        1. to display 7 to 19 all numbers, 
+        2. and only display odd numbers
+        ==== 7.
+--------------------------------------*/
+   
+  var i = 7;
+  var arr1=[];
+  var arr2=[];
+  while (i <= 19) {
+    arr1.push(i);
+    if(i%2!=0)arr2.push(i);
+    i++;
+  }
+ 
+  for (const items of arr1) {
+      console.log(items);  //expected output: [7,8,9,10,11,12,13,14,15,16,17,18,19]
+  }
+  for (const items of arr2) {
+    console.log(items); //expected output: [7,9,11,13,15,17,19]
+}
+  
+/*-------------------------------------
+    (i) Declare an array,
+    (ii) show the number of elements,
+    (iii) update or change the 4th position of the elements,
+    (iv) add & remove an element,
+    (v) check whether a specific value is exists in the array
+        ==== 8.
+--------------------------------------*/
+  var array = [];//Declare an array,
+  var i=0;
+  while (i <= 19) {
+    array.push(Math.round(Math.random()*100));
+    i++;
+  }
+  console.log("Number of elements : ", array.length);//show the number of elements,
+  console.log("Array : ",array);
+  
+  array[3] = "4th element";
+  
+  console.log("after uptated 4th position its changed to: ", array[3]);
+  /* different methods you can use to remove elements from JavaScript arrays:
+  pop - Removes from the End of an Array
+  shift - Removes from the beginning of an Array
+  splice - removes from a specific Array index
+  filter - allows you to programatically remove elements from an Array */
+  array.pop();
+  
+  let foundNumber = false;
+  var index = -1;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] == Math.round(Math.random()*100)) {
+        foundNumber = true;
+        index = i;
+    }
+  }
+  if (foundNumber) {
+    console.log("Found the Number.Index : ",i);
+  } else {
+    console.log("Sorry ! Does not found the Number");
+  }
+   array.splice(3);//delete 17 elemnts in array after 4th element.
+/*-------------------------------------
+    display all elements of the array using for loop    ==== 9.
+--------------------------------------*/
+  for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+  }
+/*-------------------------------------
+    Display only the numbers bigger than 50     ==== 11.
+--------------------------------------*/
+  var arrayOfNumbers = [];
+  var s = 0;
+  while (s <= 19) {
+    arrayOfNumbers.push(Math.round(Math.random()*100));
+    s++;
+  }
+  for (let i = 0; i < arrayOfNumbers.length; i++) {
+    if (arrayOfNumbers[i] > 50) {
+      console.log("The number bigger than 50 is :", arrayOfNumbers[i]);
+    }
+  }
+/*-------------------------------------
+Three numbers as parameters and returns the multiplication of that parameters.    ==== 11.
+--------------------------------------*/
+  function multiplication(n1, n2, n3) {
+    return n1 * n2 * n3;
+  }
+  console.log("Multiplication of three parameters are :",multiplication(2, 3, 7));
+/*-------------------------------------
+    Declare an object and change any property of that object     ==== 12.
+--------------------------------------*/
+
+  var person = {
+    name: "Md Saifur Rahman",
+    age: 23,
+    id: "C183027",
+    height: "5ft-10inch",
+    education: "Bsc in CSE",
+    university: "iiuc",
+  };
+  person.education = "Bachelor of Science in Computer Science & Software Engineering"; //change education property
+  person.university = "International Islamic University Chittagong"; //change education property
+  console.log(person);
+  for (const datakey of Object.keys(person)) {
+      console.log(datakey);
+  }
+  for (const value of Object.values(person)) {
+      console.log(value);
+  }
